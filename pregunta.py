59bd4b2749239ed25d55829c78c5dd50ee3a9a17
dialarconb <dialarconb@unal.edu.co>
2023-10-29 18:33:29 -0500

    taller5_V3

--- pregunta.py
+++ pregunta.py
@@ -13,37 +13,38 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    column_names = ["id","sexo","tipo_de_emprendimiento","idea_negocio","barrio","estrato","comuna_ciudadano","fecha_de_beneficio","monto_del_credito","línea_credito"]
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", skiprows=1,names=column_names)
+    #
+    # Inserte su código aquí
+    #
+    df= df[df.columns[1:]]
+    df.columns.values
 
-    #Elimina la col id
-    df = df.drop('id', axis=1)
+    df['sexo']= df['sexo'].str.lower() 
 
-    #Convertir todo a minusculas
-    df = df.applymap(lambda s: s.lower() if type(s) == str else s)
-    
-    # Date check  
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
-    
-    df.dropna(axis='index',inplace=True)
+    df['tipo_de_emprendimiento']= df['tipo_de_emprendimiento'].str.lower()
+
+    df['idea_negocio']= df['idea_negocio'].str.lower()
+    df['idea_negocio']=df['idea_negocio'].str.replace("-","_")
+    df['idea_negocio']=df['idea_negocio'].str.replace("_"," ")
+    df['idea_negocio']=df['idea_negocio'].str.strip()
+
+    df['barrio']= df['barrio'].str.lower()
+    df['barrio']=df['barrio'].str.replace("-","_")
+    df['barrio']=df['barrio'].str.replace("_"," ")
+
+    df['línea_credito']= df['línea_credito'].str.lower()
+    df['línea_credito']=df['línea_credito'].str.replace("-","_")
+    df['línea_credito']=df['línea_credito'].str.replace("_"," ")
+    df['línea_credito']=df['línea_credito'].str.replace(".","")
+    df['línea_credito']=df['línea_credito'].str.strip()
+
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,dayfirst=True)
+
+    df['monto_del_credito']=df['monto_del_credito'].str.replace(",","")
+    df['monto_del_credito']=df['monto_del_credito'].str.replace("$","")
+    df['monto_del_credito']=df['monto_del_credito'].str.strip()
+    df['monto_del_credito'] = df['monto_del_credito'].apply(pd.to_numeric, downcast="integer", errors='ignore')
     df.drop_duplicates(inplace=True)
-
-    # Lowercase check
-    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
-    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
-    df['barrio'] = df['barrio'].str.lower().astype(str)
-    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
-     
-    # Whitespaces, hyphen and underscore checks
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
-
-    # Money check
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)
-
-    df.drop_duplicates(inplace=True)
-    df.dropna(axis='index',inplace=True)
-
+    df.dropna(inplace=True)
+    
     return df
